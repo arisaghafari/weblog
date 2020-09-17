1d36b6a93d63b3a54358266cd13c97cb0b91a48b
arisaghafari <arisa_ghafari@outlook.com>
2020-09-17 18:45:38 +0430

    add a bootstrap template

--- website/blog/models.py
+++ website/blog/models.py
@@ -3,17 +3,20 @@ from django.utils import timezone
 
 class Article(models.Model):
     STATU_CHOICES = (
-        ('d', 'Draft'),
-        ('p', 'Published'),
+        ('d', 'پیش نویس'),
+        ('p', 'منتشر شده'),
     )
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to="images")
-    publish = models.DateTimeField(default=timezone.now())
+    title = models.CharField(max_length=200, verbose_name="عنوان مقاله")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="ادرس مقاله")
+    description = models.TextField(verbose_name="محتوا")
+    thumbnail = models.ImageField(upload_to="images", verbose_name="تصویر مقاله")
+    publish = models.DateTimeField(default=timezone.now(), verbose_name="زمان انتشار")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATU_CHOICES)
+    status = models.CharField(max_length=1, choices=STATU_CHOICES, verbose_name="وضعیت")
 
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
     def __str__(self):
         return self.title
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Article
 
 def home(request):
     context = {
-        "articles" : Article.objects.filter(status="p").order_by("-publish")[:5]
+        "articles" : Article.objects.filter(status="p").order_by("-publish")
     }
     return render(request, "blog/home.html", context)
 
 def detailArticle(request, slug):
     context = {
-        "article" : Article.objects.get(slug = slug)
+        "article" : get_object_or_404(Article, slug, status = "p")
     }
     return render(request, "blog/detailArticle.html", context)
