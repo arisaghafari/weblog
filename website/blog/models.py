c912b228d954bbb66a0839da0340607721e04d90
arisaghafari <arisa_ghafari@outlook.com>
2020-09-18 00:52:31 +0430

    add category model

--- website/blog/models.py
+++ website/blog/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.utils import timezone
 from extensions.utils import jalali_converter
 
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان دسته بندی")
+    slug = models.SlugField(max_length=200, unique=True, verbose_name="ادرس دسته بندی")
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField(verbose_name="وضعیت")
+
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+        ordering = ["position"]
+
+    def __str__(self):
+        return self.title
+
 class Article(models.Model):
     STATU_CHOICES = (
         ('d', 'پیش نویس'),
@@ -15,10 +29,11 @@ class Article(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, choices=STATU_CHOICES, verbose_name="وضعیت")
-
+    category = models.ManyToManyField(Category, verbose_name="دسته بندی")
     class Meta:
         verbose_name = "مقاله"
         verbose_name_plural = "مقالات"
+        ordering = ["-publish"]
 
     def jpublish(self):
         return jalali_converter(self.publish)
@@ -26,3 +41,4 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render , get_object_or_404
-from .models import Article
+from .models import Article, Category
 
 def home(request):
     context = {
-        "articles" : Article.objects.filter(status="p").order_by("-publish")
+        "articles" : Article.objects.filter(status="p"),
+        "categoris" : Category.objects.filter(status=True)
     }
     return render(request, "blog/home.html", context)
 
 def detailArticle(request, slug):
     context = {
-        "article" : get_object_or_404(Article, slug = slug, status="p")
+        "article" : get_object_or_404(Article, slug = slug, status="p"),
+        "categoris": Category.objects.filter(status=True)
     }
     return render(request, "blog/detailArticle.html", context)
