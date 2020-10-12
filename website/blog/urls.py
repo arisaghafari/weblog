f5b9b4d62c6c985dfd053f9e5ba811250b5f6c02
arisaghafari <arisa_ghafari@outlook.com>
2020-10-12 22:46:40 +0330

    add ListView instead of home methone in view file

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 app_name = "blog"
 urlpatterns = [
-    path('', home, name = "home"),
-    path('page/<int:page>', home, name = "home"),
+    path('', ArticleList.as_view(), name = "home"),
+    path('page/<int:page>', ArticleList.as_view(), name = "home"),
     path('article/<slug:slug>', detailArticle, name = "detailArticle"),
     path('category/<slug:slug>', categoryArticle, name = "categoryArticle"),
     path('category/<slug:slug>/page/<int:page>', categoryArticle, name = "categoryArticle")
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Article, Category
 from django.core.paginator import Paginator
+from django.views.generic.list import ListView
 
+"""
 def home(request, page=1):
     articleList = Article.objects.published()
     paginator = Paginator(articleList, 3)
@@ -11,6 +13,12 @@ def home(request, page=1):
         "articles" : articles
     }
     return render(request, "blog/home.html", context)
+"""
+class ArticleList(ListView):
+    paginate_by = 3
+    #context_object_name = "articles"
+    template_name = "blog/home.html"
+    queryset = Article.objects.published()
 
 def detailArticle(request, slug):
     context = {
