4244e46cb2a1aba9ec4862194aaa50589cbe4a0f
arisaghafari <arisa_ghafari@outlook.com>
2020-10-12 23:01:16 +0330

    add detail view instead of detailArticle method

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -5,7 +5,7 @@ app_name = "blog"
 urlpatterns = [
     path('', ArticleList.as_view(), name = "home"),
     path('page/<int:page>', ArticleList.as_view(), name = "home"),
-    path('article/<slug:slug>', detailArticle, name = "detailArticle"),
+    path('article/<slug:slug>', ArticleDetail.as_view(), name = "detailArticle"),
     path('category/<slug:slug>', categoryArticle, name = "categoryArticle"),
     path('category/<slug:slug>/page/<int:page>', categoryArticle, name = "categoryArticle")
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from .models import Article, Category
 from django.core.paginator import Paginator
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 
 """
 def home(request, page=1):
@@ -14,17 +14,25 @@ def home(request, page=1):
     }
     return render(request, "blog/home.html", context)
 """
+
 class ArticleList(ListView):
     paginate_by = 3
     #context_object_name = "articles"
-    template_name = "blog/home.html"
+    #template_name = "blog/home.html"
     queryset = Article.objects.published()
 
+"""
 def detailArticle(request, slug):
     context = {
         "article" : get_object_or_404(Article.objects.published(), slug = slug)
     }
     return render(request, "blog/detailArticle.html", context)
+"""
+class ArticleDetail(DetailView):
+
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(), slug = slug)
 
 def categoryArticle(request, slug, page=1):
     category = get_object_or_404(Category, slug=slug, status=True)
